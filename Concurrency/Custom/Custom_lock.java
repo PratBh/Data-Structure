7b35e273a02ef3cc568ffb4cf02a9414a23bfadd
PratBh <34206168+PratBh@users.noreply.github.com>
2021-03-19 21:22:57 +0530

    Update Custom_lock.java

--- Concurrency/Custom/Custom_lock.java
+++ Concurrency/Custom/Custom_lock.java
@@ -1,1 +1,20 @@
+package Custom;
+
+public class Custom_lock {
+	private boolean isLocked;
+	public Custom_lock() {
+		this.isLocked=false;
+	}
+	
+	public synchronized void lock() throws InterruptedException {
+		while(this.isLocked)
+			wait();
+		this.isLocked=true;
+	}
+	
+	public synchronized void unlock() {
+		this.isLocked=false;
+		notify();
+	}
+}
 
