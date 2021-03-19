4960e2a813d4edee3be906088d282d59ad836c33
PratBh <34206168+PratBh@users.noreply.github.com>
2021-03-19 21:19:06 +0530

    Update Test.java

--- BinarySearch/AdityaVerma/Test.java
+++ BinarySearch/AdityaVerma/Test.java
@@ -1,1 +1,25 @@
 
+package adityaVerma;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Problems prb=new Problems();
+		int[]arr= {12, 34, 67, 90};
+		char[] alp= {'a','c','d','g','h'};
+		//prb.binarySearchReverse(arr, 5);
+		//prb.agnosticSearch(arr, 3);
+		//prb.firstANdLastOccurance(arr, 5);
+		//prb.countOccurance(arr, 4);
+		//prb.numberOfRotation(arr);
+		//prb.findElementInRotatedArr(arr, 2);
+		//prb.findCeil(arr, 13);
+		//prb.findNextAlp(alp, 'f');
+		//prb.firstOccuranceInfinite(arr);
+		//prb.findElementMinDiff(arr, 7);
+		//prb.peakElementIndex(arr);
+		//prb.findElementBitonic(arr, 20);
+		prb.allocatePages(arr, 2);
+	}
+
+}
