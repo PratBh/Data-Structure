427f0657df772dbf2607e0b1b606d9c91d8f87c3
PratBh <34206168+PratBh@users.noreply.github.com>
2021-03-19 21:23:24 +0530

    Update Custom_reentranReadWriteLock.java

--- Concurrency/Custom/Custom_reentranReadWriteLock.java
+++ Concurrency/Custom/Custom_reentranReadWriteLock.java
@@ -1,1 +1,32 @@
+package Custom;
+
+public class Custom_readWriteLock {
+	private int readers=0;
+	private int writers=0;
+	private int writeRequests=0;
+	
+	public synchronized void readLock() throws InterruptedException {
+		if(writers>0||writeRequests>0)
+			wait();
+		readers++;
+	}
+	
+	public synchronized void readUnlock() {
+		readers--;
+		notifyAll();
+	}
+	
+	public synchronized void writeLock() throws InterruptedException {
+		writeRequests++;
+		if(readers>0||writers>0)
+			wait();
+		writeRequests--;
+		writers++;
+	}
+	
+	public synchronized void writeUnlock() {
+		writers--;
+		notifyAll();
+	}
+}
 
