71cc864bdcfac5f86766c8368391aff808e1f9c6
PratBh <34206168+PratBh@users.noreply.github.com>
2021-03-19 21:20:19 +0530

    Update test.java

--- BinarySearch/book/test.java
+++ BinarySearch/book/test.java
@@ -1,1 +1,11 @@
 
+package book;
+
+
+public class Test {
+	public static void main(String[] args) {
+		Problems prb=new Problems();
+		int[]arr= {3,2,1,2,2,3};
+		prb.findFirstNumRepeated(arr);
+	}
+}
