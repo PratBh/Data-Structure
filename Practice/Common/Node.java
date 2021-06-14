099286bbd9dc85dfa7f055172435740b3c8ced2e
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:42:01 +0530

    Update Node.java

--- Practice/Common/Node.java
+++ Practice/Common/Node.java
@@ -1,1 +1,38 @@
+import java.util.ArrayList;
+
+public class Node {
+	public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+ 
+    
+    public Node flatten(Node head,ArrayList<Integer> res) {
+        if(head == null)
+            return null;
+        res.add(head.val);
+        if(head.child != null){
+            Node earlyNext = head.next;
+            if(earlyNext!=null)
+            earlyNext.prev=null;
+            Node child = flatten(head.child,res);
+            head.next=child;
+            while(child.next != null){
+                child=child.next;
+            }
+            if(earlyNext !=null){
+                earlyNext.prev=child;
+            child.next=flatten(earlyNext,res);
+            }
+            return head;
+        }else if(head.child == null && head.next== null) {
+            return head;
+        }
+        else{
+            head.next=flatten(head.next,res);
+            return head;
+            }
+            
+        }
+}
 
