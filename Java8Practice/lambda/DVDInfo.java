433d8b06fdd49b060f64957a3dca2588b1ade959
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:37:25 +0530

    Create DVDInfo.java

---
+++ Java8Practice/lambda/DVDInfo.java
@@ -0,0 +1,25 @@
+package lambda;
+
+public class DVDInfo {
+	String title;
+	String genre;
+	public DVDInfo(String title, String genre) {
+		super();
+		this.title = title;
+		this.genre = genre;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
+}
