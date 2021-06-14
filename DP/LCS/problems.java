f7f264d3a6608a53802c15b844f31d7644897c83
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 12:57:00 +0530

    Update problems.java

--- DP/LCS/problems.java
+++ DP/LCS/problems.java
@@ -1,6 +1,8 @@
 package LCS;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class problrms {
 	
@@ -278,6 +280,203 @@ public class problrms {
 	int minInsertToMakePalindrome(String a) {
 		return a.length()-LPS(a);
 	}
-
+//	You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this
+//	place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected,
+//	and it will automatically contact the police if two adjacent houses were broken into on the same night.
+//
+//	Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting 
+//			the police.
+//
+//	 
+//
+//	Example 1:
+//
+//	Input: nums = [2,3,2]
+//	Output: 3
+//	Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+//	Example 2:
+//
+//	Input: nums = [1,2,3,1]
+//	Output: 4
+//	Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+//	Total amount you can rob = 1 + 3 = 4.
+//	Example 3:
+//
+//	Input: nums = [0]
+//	Output: 0
+	//https://leetcode.com/problems/house-robber-ii/discuss/1151554/Exactly-same-as-House-Robber
+	public int rob(int[] nums) {
+		if(nums.length==0)
+			return 0;
+		if(nums.length==1)
+			return nums[0];
+		if(nums.length==2)
+			Math.max(nums[0], nums[1]);
+        int n=nums.length;
+        int []dp=new int[nums.length+1];
+        Arrays.fill(dp, -1);
+        //for the part 0 to n-1
+        dp[n-1]=0;
+        dp[n-2]=nums[n-2];
+        for(int i=n-3;i>=0;i--) {
+        	dp[i]=Math.max(nums[i]+dp[i+2], dp[i+1]);
+        }
+        int c1=dp[0];
+        
+        Arrays.fill(dp, -1);
+        //for the part 1 to n
+        dp[n]=0;
+        dp[n-1]=nums[n-1];
+        for(int i=n-2;i>=1;i--)
+        	dp[i]=Math.max(nums[i]+dp[i+2], dp[i+1]);
+        int c2=dp[1];
+        return Math.max(c1, c2);
+    }
+//	The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+//
+//	Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses in this place form a 
+//	binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+//
+//	Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+//
+//	 
+//
+//	Example 1:
+//
+//
+//	Input: root = [3,2,3,null,3,null,1]
+//	Output: 7
+//	Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+//	Example 2:
+//
+//
+//	Input: root = [3,4,5,1,3,null,1]
+//	Output: 9
+//	Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+//	 
+//
+//	Constraints:
+//
+//	The number of nodes in the tree is in the range [1, 104].
+//	0 <= Node.val <= 104
+//			https://leetcode.com/problems/house-robber-iii/
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode() {}
+		      TreeNode(int val) { this.val = val; }
+		      TreeNode(int val, TreeNode left, TreeNode right) {
+		          this.val = val;
+		          this.left = left;
+		          this.right = right;
+		      }
+		  }
+	public int rob(TreeNode root) {
+		int []res=robUtil(root);
+		return Math.max(res[0], res[1]);
+	}
+	
+	public int[]robUtil(TreeNode node){
+		if(node==null)
+			return new int[2];
+		int []left=robUtil(node.left);
+		int []right=robUtil(node.right);
+		
+		int ans[]=new int[2];
+		ans[0]=left[1]+node.val+right[1];
+		ans[1]=Math.max(left[0], left[1])+Math.max(right[0], right[1]);
+		return ans;
+	}
+		 
+//		   public int rob(TreeNode root) {
+//		       int c1=0,c2=0,parent=0;
+//		       Queue<TreeNode> q=new LinkedList<problrms.TreeNode>();
+//		       q.add(root);
+//		       q.add(null);
+//		       while(!q.isEmpty()) {
+//		    	   TreeNode temp=q.poll();
+//		    	   if(temp!=null) {
+//		    		   if(parent==0)
+//		    			   c1+=temp.val;
+//		    		   else
+//		    			   c2+=temp.val;
+//		    		   if(temp.left!=null)
+//		    			   q.add(temp.left);
+//		    		   if(temp.right!=null)
+//		    			   q.add(temp.right);
+//		    	   }else {
+//		    		   if(!q.isEmpty()) {
+//		    			   parent=parent-1;
+//		    			   q.add(null);
+//		    		   }
+//		    	   }
+//		       }
+//		       return Math.max(c1,c2);
+//		   }
+	
+	//Count the number of prime numbers less than a non-negative number, n.
+	
+	public int countPrimes(int n) {
+        if(n==0)
+        	return 0;
+        boolean prime[]=new boolean[n+1];
+        Arrays.fill(prime, true);
+        int count=0;
+        for(int i=2;i<=n;i++) {
+        	if(prime[i]) {
+        		count++;
+        		for(int j=i*i;j<=n;j+=i)
+        			prime[j]=false;
+        	}
+        }
+        for(int i=2;i<=n;i++) {
+        	if(prime[i])
+        		System.out.println(i);
+        }
+        return count;
+    }
+	
+	//Find the highest occurring digit in prime numbers in a range
+	//https://www.geeksforgeeks.org/find-highest-occurring-digit-prime-numbers-range/
+	public int maxDigitInPrimes(int start, int end) {
+		boolean[] prime=new boolean[end+1];
+		Arrays.fill(prime, true);
+		for(int i=2;i<=end;i++) {
+        	if(prime[i]) {
+        		for(int j=i*i;j<=end;j+=i)
+        			prime[j]=false;
+        	}
+        }
+		
+		int[] freq=new int[10];
+		for(int i=start;i<=end;i++) {
+			if(prime[i]) {
+				while(i>0) {
+					freq[i%10]++;
+					i=i/10;
+				}
+			}
+		}
+		
+		int max=freq[0],ans=0;
+		for(int j=1;j<10;j++) {
+			if(freq[j]>max) {
+				max=freq[j];
+				ans=j;
+			}
+		}
+		return ans;
+	}
+	
+	//find lcm of two numbers
+	int lcm(int a,int b) {
+		int gcd=1,lcm;
+		for(int i=1;i<=(a<b?a:b);i++) {
+			if(a%i==0 && b%i==0)
+				gcd=i;
+		}
+		lcm=(a*b)/gcd;
+		return lcm;
+	}
 }
-
