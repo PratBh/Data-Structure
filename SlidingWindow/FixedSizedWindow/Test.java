3096dfdc298a16c26bdd0e6c87f800575226b5c9
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:06:59 +0530

    Update Test.java

--- SlidingWindow/FixedSizedWindow/Test.java
+++ SlidingWindow/FixedSizedWindow/Test.java
@@ -1,1 +1,15 @@
 
+package fixedSizeWindow;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Problems prb=new Problems();
+		int[] arr= {1,3,-1,-3,5,3,6,7};
+		//prb.maxSumSubarray(arr, 3);
+		//prb.firstNegative(arr, 3);
+		//prb.countAnagram("forofm", "for");
+		prb.maxOfSubarray(arr, 3);
+	}
+
+}
