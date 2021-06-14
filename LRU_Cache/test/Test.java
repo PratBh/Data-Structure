3d71261b756949d9925d5ad79e55d8c0e2bc350e
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:40:54 +0530

    Create Test.java

---
+++ LRU_Cache/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		LRU_withoutLinked cache=new LRU_withoutLinked(4);
+//		cache.refer(1);
+//		cache.refer(2);
+//		cache.refer(3);
+//		cache.refer(4);
+//		cache.refer(5);
+//		cache.display();
+		final StringBuffer sb=new StringBuffer("Potla");
+		sb.append("potoleibcccttbdtenuhrghhkvgvkdjtrccjdbethbdhtlctgeibcccttbdtecdjneiidbrfrnhgitkhebccrljrdvlcneibcccttbdterdfuvlrtekcudkuithihcvnnnltthnrr");
+		System.out.println(sb);
+	}
+
+}
