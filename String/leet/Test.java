c146d6cc5bf3ddc01ff553cf8bef6f0647c68143
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:14:29 +0530

    Create Test.java

---
+++ String/leet/Test.java
@@ -0,0 +1,13 @@
+package leet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Problems prb=new Problems();
+		//prb.interpret("G()()()()(al)");
+		//prb.balancedStringSplit("RLLLLRRRLR");
+		//prb.sortString("aaaabbbbcccc");
+		System.out.println(prb.generatePalindrome(5,3));
+	}
+
+}
