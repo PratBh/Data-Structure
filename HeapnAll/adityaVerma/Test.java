dcb869c3904b4bf30eb059a81fbc58fdfee9f3f2
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:00:28 +0530

    Update Test.java

--- HeapnAll/adityaVerma/Test.java
+++ HeapnAll/adityaVerma/Test.java
@@ -1,1 +1,18 @@
 
+package com.aditya.verma.heap;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HeapPractice hp=new HeapPractice();
+		int [] arr= {1,2,3,4,5};
+		//hp.kthSamllest(3, arr);
+		//hp.kLargestNumbers(3, arr);
+		//hp.sortKSorted(3, arr);
+		//hp.kClosest(3, arr, 7);
+		//hp.kFrequent(2, arr);
+		//hp.freqSort(arr);
+		hp.minCost(arr);
+	}
+
+}
