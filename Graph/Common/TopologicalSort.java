64dad4d58e3842fd4dbd3aebb729f320594f91ef
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 14:58:07 +0530

    Update TopologicalSort.java

--- Graph/Common/TopologicalSort.java
+++ Graph/Common/TopologicalSort.java
@@ -1,1 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TopologicalSort {
+	public Deque<VertexNew<Integer>> topSort(Graph<Integer> graph) {
+	Deque<VertexNew<Integer>> st=new ArrayDeque<>();
+	Set<VertexNew<Integer>> visited = new HashSet<>();
+	for(VertexNew<Integer> vertex:graph.getAllVertexNew()) {
+		if(visited.contains(vertex))
+			continue;
+		topSortUtil(vertex, st, visited);	
+		}
+		return st;
+	}
+	
+	private void topSortUtil(VertexNew vertex, Deque<VertexNew<Integer>> stack,
+			Set<VertexNew<Integer>> visited) {
+		visited.add(vertex);
+		for(VertexNew<Integer> child : vertex.getAdjacentVertexNewes()) {
+			if(visited.contains(child))
+				continue;
+			topSortUtil(child, stack, visited);
+		}
+		stack.offerFirst(vertex);
+	}
+}
 
