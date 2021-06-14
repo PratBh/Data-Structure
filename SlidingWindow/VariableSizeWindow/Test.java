c10e4216aafc2301455dfd4d4a43374fe28e3fa6
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:07:49 +0530

    Update Test.java

--- SlidingWindow/VariableSizeWindow/Test.java
+++ SlidingWindow/VariableSizeWindow/Test.java
@@ -1,1 +1,13 @@
+package variableSizeWindow;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int [] arr= {-5,8,-13,2,6,12};
+		Problems prb=new Problems();
+		//prb.subarraySumKNeg(-5, arr);
+		prb.substringKUnique("aabacbebebe", 3);
+	}
+
+}
 
