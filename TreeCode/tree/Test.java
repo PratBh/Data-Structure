685786c299ba2d73ed493d0b58406fdd17e39a88
PratBh <34206168+PratBh@users.noreply.github.com>
2021-06-14 15:17:32 +0530

    Update Test.java

--- TreeCode/tree/Test.java
+++ TreeCode/tree/Test.java
@@ -1,1 +1,15 @@
+package tree;
+
+public class Test {
+
+	public static void main(String[] args) {
+		treeNode root=new treeNode(1);
+		root.leftNode=new treeNode(2);
+		root.rightNode=new treeNode(3);
+		root.leftNode.rightNode=new treeNode(5);
+		Problems prb=new Problems();
+		prb.verticalOrder(root);
+	}
+
+}
 
